d9481df89d59f22ab950bd18fbd8e55f5cde44c6
João Bispo <joaobispo@gmail.com>
2022-11-16 00:02:07 +0000

    [Parboil] Fixes bug in sgemm

--- Parboil/resources/sgemm/io.cpp
+++ Parboil/resources/sgemm/io.cpp
@@ -35,6 +35,8 @@ bool readColMajorMatrixFile(const char *fn, int &nr_row, int &nr_col, std::vecto
     v.push_back(data);
   }
   v.pop_back(); // remove the duplicated last element
+  
+  return true;
 }
 
 bool writeColMajorMatrixFile(const char *fn, int nr_row, int nr_col, std::vector<float> &v)
