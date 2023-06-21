6d7f738a2b27b00a5953adabc24125ba7feece25
tiagolascasas <tiagolascasas@outlook.com>
2023-06-22 00:38:31 +0100

    [Rosetta] Cleaned up face-detect

--- Rosetta/resources/face-detection/typedefs.h
+++ Rosetta/resources/face-detection/typedefs.h
@@ -16,7 +16,7 @@ const int IMAGE_HEIGHT = 240;
 const int IMAGE_WIDTH = 320;
 const int RESULT_SIZE = 100;
 const int IMAGE_MAXGREY = 255;
-const int IMAGE_SIZE = ( IMAGE_HEIGHT * IMAGE_WIDTH );
+const int IMAGE_SIZE = (IMAGE_HEIGHT * IMAGE_WIDTH);
 const int TOTAL_NODES = 2913;
 const int TOTAL_STAGES = 25;
 const int TOTAL_COORDINATES = TOTAL_NODES * 12;
@@ -29,54 +29,33 @@ const int COLS = 25;
 const int NUM_BANKS = 12;
 const int SIZE = 2913;
 
-#ifdef OCL
-  #include <string>
-  // target device
-  // change here to map to a different device
-  const std::string TARGET_DEVICE = "xilinx_aws-vu9p-f1-04261818_dynamic_5_0";
-#endif
-
-
-#ifndef SW
-  #include "ap_int.h"
-  // HLS-specific datatypes
-  typedef ap_uint<18> uint18_t;
-  typedef ap_uint<10> uint10_t;
-  typedef ap_uint<1>  bit;
-  typedef ap_uint<5> uint5_t;
-  typedef ap_uint<13> int_I;  
-  typedef ap_uint<21> int_SI;
-  typedef ap_uint<18> int_II;             
-  typedef ap_uint<26> int_SII;
-#endif
-
 // standard datatypes
 typedef struct MyPoint
 {
-  int x;
-  int y;
+    int x;
+    int y;
 } MyPoint;
 
 typedef struct
 {
-  int width;
-  int height;
+    int width;
+    int height;
 } MySize;
 
 typedef struct
 {
-  int x; 
-  int y;
-  int width;
-  int height;
+    int x;
+    int y;
+    int width;
+    int height;
 } MyRect;
 
-typedef struct 
+typedef struct
 {
-  int width;
-  int height;
-  int maxgrey;
-  int flag;
+    int width;
+    int height;
+    int maxgrey;
+    int flag;
 } MyInputImage;
 
 #endif
