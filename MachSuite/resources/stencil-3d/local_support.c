4fbf849f53188734c4e3aa5a7b6b47a7ea97203e
tiagolascasas <tiagolascasas@outlook.com>
2023-10-21 03:04:01 +0100

    Adds missing MachSuite benchmarks

--- MachSuite/resources/stencil-3d/local_support.c
+++ MachSuite/resources/stencil-3d/local_support.c
@@ -8,32 +8,34 @@ int INPUT_SIZE = sizeof(struct bench_args_t);
 void run_benchmark(void *vargs)
 {
     struct bench_args_t *args = (struct bench_args_t *)vargs;
-    stencil(args->orig, args->sol, args->filter);
+    printf("Args: %d %d\n", args->orig[0], args->orig[1]);
+    printf("%d %d %d\n", args->orig[0], args->orig[1], args->orig[2]);
+    printf("%d %d %d\n", args->sol[0], args->sol[1], args->sol[2]);
+    stencil3d(args->C, args->orig, args->sol);
+    printf("%d %d %d\n", args->sol[0], args->sol[1], args->sol[2]);
 }
 
 /* Input format:
 %% Section 1
-TYPE[row_size*col_size]: input matrix
+TYPE[2]: stencil coefficients (inner/outer)
 %% Section 2
-TYPE[f_size]: filter coefficients
+TYPE[SIZE]: input matrix
 */
 
 void input_to_data(int fd, void *vdata)
 {
     struct bench_args_t *data = (struct bench_args_t *)vdata;
     char *p, *s;
-    // Zero-out everything.
-    memset(vdata, 0, sizeof(struct bench_args_t));
     // Load input string
     p = readfile(fd);
 
     s = find_section_start(p, 1);
     STAC(parse_, TYPE, _array)
-    (s, data->orig, row_size * col_size);
+    (s, data->C, 2);
 
     s = find_section_start(p, 2);
     STAC(parse_, TYPE, _array)
-    (s, data->filter, f_size);
+    (s, data->orig, SIZE);
     free(p);
 }
 
@@ -43,30 +45,28 @@ void data_to_input(int fd, void *vdata)
 
     write_section_header(fd);
     STAC(write_, TYPE, _array)
-    (fd, data->orig, row_size * col_size);
+    (fd, data->C, 2);
 
     write_section_header(fd);
     STAC(write_, TYPE, _array)
-    (fd, data->filter, f_size);
+    (fd, data->orig, SIZE);
 }
 
 /* Output format:
 %% Section 1
-TYPE[row_size*col_size]: solution matrix
+TYPE[SIZE]: solution matrix
 */
 
 void output_to_data(int fd, void *vdata)
 {
     struct bench_args_t *data = (struct bench_args_t *)vdata;
     char *p, *s;
-    // Zero-out everything.
-    memset(vdata, 0, sizeof(struct bench_args_t));
     // Load input string
     p = readfile(fd);
 
     s = find_section_start(p, 1);
     STAC(parse_, TYPE, _array)
-    (s, data->sol, row_size * col_size);
+    (s, data->sol, SIZE);
     free(p);
 }
 
@@ -76,7 +76,7 @@ void data_to_output(int fd, void *vdata)
 
     write_section_header(fd);
     STAC(write_, TYPE, _array)
-    (fd, data->sol, row_size * col_size);
+    (fd, data->sol, SIZE);
 }
 
 int check_data(void *vdata, void *vref)
@@ -84,16 +84,13 @@ int check_data(void *vdata, void *vref)
     struct bench_args_t *data = (struct bench_args_t *)vdata;
     struct bench_args_t *ref = (struct bench_args_t *)vref;
     int has_errors = 0;
-    int row, col;
+    int i;
     TYPE diff;
 
-    for (row = 0; row < row_size; row++)
+    for (i = 0; i < SIZE; i++)
     {
-        for (col = 0; col < col_size; col++)
-        {
-            diff = data->sol[row * col_size + col] - ref->sol[row * col_size + col];
-            has_errors |= (diff < -EPSILON) || (EPSILON < diff);
-        }
+        diff = data->sol[i] - ref->sol[i];
+        has_errors |= (diff < -EPSILON) || (EPSILON < diff);
     }
 
     // Return true if it's correct.
