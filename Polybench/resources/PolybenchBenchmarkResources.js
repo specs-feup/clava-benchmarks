54fbf1f83176b6e2e6e030526145200ead9ea9c9
L. Sousa <lm.sousa@fe.up.pt>
2024-08-02 19:35:39 +0100

    Deprecate usage of lara.resource files

---
+++ Polybench/resources/PolybenchBenchmarkResources.js
@@ -0,0 +1,15 @@
+import LocalFolder from "lara-js/api/lara/util/LocalFolder.js";
+import { Engine, engine } from "lara-js/api/lara/util/JavaTypes.js";
+
+export default class PolybenchBenchmarkResources extends LocalFolder {
+    constructor() {
+        let filepath;
+        if (engine == Engine.GraalVM) {
+            filepath = __dirname;
+        } else {
+            filepath = import.meta.dirname;
+        }
+
+        super(filepath);
+    }
+}
