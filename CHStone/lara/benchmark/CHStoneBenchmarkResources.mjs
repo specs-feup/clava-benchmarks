bf5ce887f01d1baa064ceb1258ab4748cd286c52
João Bispo <jbispo@fe.up.pt>
2024-08-08 20:17:59 +0100

    Changes CHStoneBenchmarkResources to not use constructor

--- CHStone/lara/benchmark/CHStoneBenchmarkResources.mjs
+++ CHStone/lara/benchmark/CHStoneBenchmarkResources.mjs
@@ -3,14 +3,19 @@ import { Engine, engine } from "lara-js/api/lara/util/JavaTypes.js";
 import System from "lara-js/api/lara/System.js";
 
 export default class CHStoneBenchmarkResources extends LocalFolder {
-  constructor() {
+  //constructor() {
+  //super();
+  //super(System.getCurrentFolder());
+  /*
     let filepath;
     if (engine == Engine.GraalVM) {
       filepath = System.getCurrentFolder();
     } else {
+      console.log("Node path");
       filepath = import.meta.dirname;
     }
-
+    console.log("File path: " + filepath);
     super(filepath);
-  }
+    */
+  //}
 }
