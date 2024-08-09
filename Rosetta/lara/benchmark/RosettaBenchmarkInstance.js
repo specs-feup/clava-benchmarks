d0d697e1307383335838eab212960f42eb37ccc3
João Bispo <jbispo@fe.up.pt>
2024-08-09 12:04:21 +0100

    Updates Rosetta to support Lara-JS

--- Rosetta/lara/benchmark/RosettaBenchmarkInstance.js
+++ Rosetta/lara/benchmark/RosettaBenchmarkInstance.js
@@ -6,141 +6,141 @@ laraImport("clava.ClavaJoinPoints");
 
 laraImport("weaver.Query");
 
-/**
- * Instance of a C Rosetta benchmark.
- */
-function RosettaBenchmarkInstance(benchmarkName, inputSize) {
+class RosettaBenchmarkInstance extends ClavaBenchmarkInstance {
+  /**
+   * Instance of a C Rosetta benchmark.
+   */
+  constructor(benchmarkName, inputSize) {
     // Parent constructor
-	var fullName = "Rosetta-" + benchmarkName + "-" + inputSize;
-    ClavaBenchmarkInstance.call(this, fullName);
-	
-	this._benchmarkName = benchmarkName;
-	this._inputSize = inputSize;
-	
-	this._previousStandard = undefined;
-	this._previousFlags = undefined;
+    var fullName = "Rosetta-" + benchmarkName + "-" + inputSize;
+    super(fullName);
 
-	// Add lib m
-	this.getCMaker().addLibs("m");	
-}
+    this.resources = new RosettaBenchmarkResources();
 
+    this._benchmarkName = benchmarkName;
+    this._inputSize = inputSize;
 
-// Inheritance
-RosettaBenchmarkInstance.prototype = Object.create(ClavaBenchmarkInstance.prototype);
+    this._previousStandard = undefined;
+    this._previousFlags = undefined;
 
+    // Add lib m
+    this.getCMaker().addLibs("m");
+  }
 
-/*** IMPLEMENTATIONS ***/
+  /*** IMPLEMENTATIONS ***/
 
-RosettaBenchmarkInstance.prototype._loadPrologue = function() {
+  loadPrologue() {
+    // Set standard
+    this._previousStandard = Clava.getData().getStandard();
+    Clava.getData().setStandard("c++17");
 
-	// Set standard
-	this._previousStandard = Clava.getData().getStandard();
-	Clava.getData().setStandard("c++17");	
+    // Set define
+    this._previousFlags = Clava.getData().getFlags();
+    var modifiedFlags =
+      this._previousFlags + " -DCLASS_" + this._inputSize + " -D SW";
+    Clava.getData().setFlags(modifiedFlags);
+  }
 
-	// Set define
-	this._previousFlags = Clava.getData().getFlags();
-	var modifiedFlags = this._previousFlags + " -DCLASS_" + this._inputSize + " -D SW";
-	Clava.getData().setFlags(modifiedFlags);
+  closeEpilogue() {
+    // Restore standard
+    Clava.getData().setStandard(this._previousStandard);
+    this._previousStandard = undefined;
 
-}
+    // Restore flags
+    Clava.getData().setFlags(this._previousFlags);
+    this._previousFlags = undefined;
 
-RosettaBenchmarkInstance.prototype._loadPrivate = function() {
-	// Save current AST
-	Clava.pushAst();
-	
-	// Clean AST
-	Query.root().removeChildren();
+    // Delete input/output files
+    if (this._benchmarkName == "spam-filter") {
+      if (Io.isFile("output.txt")) {
+        Io.deleteFiles("output.txt");
+      }
 
-	// Add code
-	this._addCode();
+      if (Io.isFolder("data")) {
+        Io.deleteFolder("data");
+      }
+    } else if (this._benchmarkName == "optical-flow") {
+    } else {
+      if (Io.isFile("outputs.txt")) {
+        Io.deleteFiles("outputs.txt");
+      }
+    }
+  }
 
-	// Rebuild
-	Clava.rebuild();
+  copyDataFolder(fName) {
+    let dataFolder = this.resources.getFolder(fName);
+    let fullPath = Io.getWorkingFolder();
+    const copiedFolderPath = Io.getPath(fullPath, dataFolder.getName());
+    Io.copyFolder(dataFolder, copiedFolderPath);
+    dataFolder = copiedFolderPath;
+  }
 
-}
+  executePrivate() {
+    if (this._currentExe === undefined) {
+      throw "RosettaBenchmarkInstance.executePrivate(): no executable currently defined";
+    }
 
-RosettaBenchmarkInstance.prototype._closePrivate = function() {
-	// Restore standard
-	Clava.getData().setStandard(this._previousStandard);	
-	this._previousStandard = undefined;
+    if (this._benchmarkName == "spam-filter") {
+      var fName = this._benchmarkName + "/data";
+      this.copyDataFolder(fName);
 
-	// Restore flags
-	Clava.getData().setFlags(this._previousFlags);
-	this._previousFlags = undefined;	
+      this._currentExecutor.execute(
+        this._currentExe.getAbsolutePath(),
+        "-p",
+        "data"
+      );
+    } else if (this._benchmarkName == "optical-flow") {
+      if (this._inputSize == "current") {
+        var fName = this._benchmarkName + "/datasets/current";
+        this.copyDataFolder(fName);
 
-	// Restore previous AST
-	Clava.popAst();
+        this._currentExecutor.execute(
+          this._currentExe.getAbsolutePath(),
+          "-p",
+          "datasets/current",
+          "-o",
+          "output_current.flo"
+        );
+      }
+      if (this._inputSize == "sintel") {
+        this._currentExecutor.execute(
+          this._currentExe.getAbsolutePath(),
+          "-p",
+          "datasets/sintel_alley",
+          "-o",
+          "output_sintel.flo"
+        );
+      }
+    } else {
+      this._currentExecutor.execute(this._currentExe.getAbsolutePath());
+    }
+    return this._currentExecutor;
+  }
 
-	// Delete input/output files
-	if (this._benchmarkName == "spam-filter") {
-		Io.deleteFiles("output.txt");
-		Io.deleteFolder("data");
-	}
-	else if (this._benchmarkName == "optical-flow") {
+  addCode() {
+    // Create array with source files
+    var sourceFiles = [];
 
-	}
-	else {
-		Io.deleteFiles("outputs.txt");
-	}
-}
+    var dir = this.resources.getFolder(this._benchmarkName);
+    if (Io.isFolder(dir)) {
+      for (var file of Io.getFiles(dir)) {
+        if (
+          file.name.endsWith(".c") ||
+          file.name.endsWith(".cpp") ||
+          file.name.endsWith(".h")
+        ) {
+          sourceFiles.push(this._benchmarkName + "/" + file.name);
+        }
+      }
+    }
+    //sourceFiles.push(this._benchmarkName + "/test.cpp");
+    println(sourceFiles);
 
-RosettaBenchmarkInstance.prototype._copyDataFolder = function(fName) {	
-	let dataFolder = RosettaBenchmarkResources.getFolder(fName);
-	let fullPath = Io.getWorkingFolder();
-	const copiedFolderPath = Io.getPath(fullPath, dataFolder.getName());
-	Io.copyFolder(dataFolder, copiedFolderPath);
-	dataFolder = copiedFolderPath;
-}
-
-RosettaBenchmarkInstance.prototype._executePrivate = function() {		
-	if(this._currentExe === undefined) {
-		throw "RosettaBenchmarkInstance._executePrivate(): no executable currently defined";
-	}
-
-	if (this._benchmarkName == "spam-filter") {
-		var fName = this._benchmarkName + "/data";
-		this._copyDataFolder(fName);
-
-		this._currentExecutor.execute(this._currentExe.getAbsolutePath(), "-p", "data");
-	}
-	else if (this._benchmarkName == "optical-flow") {
-		if (this._inputSize == "current") {
-			var fName = this._benchmarkName + "/datasets/current";
-			this._copyDataFolder(fName);
-
-			this._currentExecutor.execute(this._currentExe.getAbsolutePath(), "-p",
-			"datasets/current", "-o", "output_current.flo");
-		}
-		if (this._inputSize == "sintel") {
-			this._currentExecutor.execute(this._currentExe.getAbsolutePath(), "-p",
-			"datasets/sintel_alley", "-o", "output_sintel.flo");
-		}
-	}
-	else {
-		this._currentExecutor.execute(this._currentExe.getAbsolutePath());
-	}
-	return this._currentExecutor;
-}
-
-RosettaBenchmarkInstance.prototype._addCode = function() {
-	// Create array with source files	
-	var sourceFiles = [];
-
-	var dir = RosettaBenchmarkResources.getFolder(this._benchmarkName);
-	if (Io.isFolder(dir)) {
-		for (var file of Io.getFiles(dir)) {
-			if(file.name.endsWith(".c") || file.name.endsWith(".cpp") || file.name.endsWith(".h")) {
-				sourceFiles.push(this._benchmarkName + "/" + file.name);
-			}
-		}
-	}
-	//sourceFiles.push(this._benchmarkName + "/test.cpp");
-	println(sourceFiles);
-
-	// Add files to tree
-	for(var filename of sourceFiles) {
-		var file = RosettaBenchmarkResources.getFile(filename);
-		var clavaJPFile = ClavaJoinPoints.file(file);
-		Clava.addFile(clavaJPFile);	
-	}
+    // Add files to tree
+    for (var filename of sourceFiles) {
+      var file = this.resources.getFile(filename);
+      var clavaJPFile = ClavaJoinPoints.file(file);
+      Clava.addFile(clavaJPFile);
+    }
+  }
 }
--- Rosetta/lara/benchmark/RosettaBenchmarkSet.js
+++ Rosetta/lara/benchmark/RosettaBenchmarkSet.js
@@ -8,105 +8,115 @@ laraImport("lara.util.StringSet");
 /**
  * C-version of the Rosetta benchmarks.
  */
-function RosettaBenchmarkSet() {
-	// Parent constructor
-	BenchmarkSet.call(this, "RosettaBenchmarkSet");
+class RosettaBenchmarkSet extends BenchmarkSet {
+  constructor() {
+    // Parent constructor
+    super("RosettaBenchmarkSet");
 
-	this._testBenchmarks = ["3d-rendering", "digit-recognition", "face-detection", "optical-flow", "spam-filter"];
-	this._testInputSizes = ["N", "current", "sintel"];
-}
-// Inheritance
-RosettaBenchmarkSet.prototype = Object.create(BenchmarkSet.prototype);
+    this._testBenchmarks = [
+      "3d-rendering",
+      "digit-recognition",
+      "face-detection",
+      "optical-flow",
+      "spam-filter",
+    ];
+    this._testInputSizes = ["N", "current", "sintel"];
+  }
 
-/*
- * Available benchmarks
- */
-RosettaBenchmarkSet._benchmarkNames = new PredefinedStrings("benchmark name", true, ["3d-rendering", "digit-recognition",
-	"face-detection", "optical-flow", "spam-filter"]);
+  /*
+   * Available benchmarks
+   */
+  static _benchmarkNames = new PredefinedStrings("benchmark name", true, [
+    "3d-rendering",
+    "digit-recognition",
+    "face-detection",
+    "optical-flow",
+    "spam-filter",
+  ]);
 
-/*
- * Available sizes
- */
-RosettaBenchmarkSet._inputSizes = new PredefinedStrings("input size", true, ["N", "current", "sintel"]);
+  /*
+   * Available sizes
+   */
+  static _inputSizes = new PredefinedStrings("input size", true, [
+    "N",
+    "current",
+    "sintel",
+  ]);
 
+  /**
+   * @return {lara.util.PredefinedStrings} Names of the available benchmarks.
+   */
+  static getBenchmarkNames() {
+    return RosettaBenchmarkSet._benchmarkNames;
+  }
 
-/**
- * @return {lara.util.PredefinedStrings} Names of the available benchmarks.
- */
-RosettaBenchmarkSet.getBenchmarkNames = function () {
-	return RosettaBenchmarkSet._benchmarkNames;
-}
+  /**
+   * @return {lara.util.PredefinedStrings} Available input sizes (some benchmarks might not support all sizes).
+   */
+  static getInputSizes() {
+    return RosettaBenchmarkSet._inputSizes;
+  }
 
-/**
- * @return {lara.util.PredefinedStrings} Available input sizes (some benchmarks might not support all sizes).
- */
-RosettaBenchmarkSet.getInputSizes = function () {
-	return RosettaBenchmarkSet._inputSizes;
-}
+  static isSizeSupported(benchName, inputSize) {
+    // Check if name and inputSize are valid
+    RosettaBenchmarkSet.getBenchmarkNames().test(benchName);
+    RosettaBenchmarkSet.getInputSizes().test(inputSize);
 
-RosettaBenchmarkSet.isSizeSupported = function (benchName, inputSize) {
-	// Check if name and inputSize are valid
-	RosettaBenchmarkSet.getBenchmarkNames().test(benchName);
-	RosettaBenchmarkSet.getInputSizes().test(inputSize);
+    if (benchName == "optical-flow") {
+      return inputSize == "current" || inputSize == "sintel";
+    } else {
+      return inputSize == "N";
+    }
+  }
 
-	if (benchName == "optical-flow") {
-		return (inputSize == "current" || inputSize == "sintel");
-	}
-	else {
-		return (inputSize == "N")
-	}
-}
+  setBenchmarks() {
+    this._testBenchmarks = RosettaBenchmarkSet.getBenchmarkNames().parse(
+      arrayFromArgs(arguments)
+    );
+  }
 
-RosettaBenchmarkSet.prototype.setBenchmarks = function () {
-	this._testBenchmarks = RosettaBenchmarkSet.getBenchmarkNames().parse(arrayFromArgs(arguments));
-}
+  setInputSizes() {
+    this._testInputSizes = RosettaBenchmarkSet.getInputSizes().parse(
+      arrayFromArgs(arguments)
+    );
+  }
 
-RosettaBenchmarkSet.prototype.setInputSizes = function () {
-	this._testInputSizes = RosettaBenchmarkSet.getInputSizes().parse(arrayFromArgs(arguments));
-}
+  /**
+   * Prints the current Rosetta benchmark set.
+   */
+  print() {
+    println("BenchmarkSet: " + this.getName());
+    println("Benchmark names: " + this._testBenchmarks);
+    println("Benchmark sizes: " + this._testInputSizes);
 
+    for (var benchName of this._testBenchmarks) {
+      print(benchName + ":");
 
-/**
- * Prints the current Rosetta benchmark set.
- */
-RosettaBenchmarkSet.prototype.print = function () {
-	println("BenchmarkSet: " + this.getName());
-	println("Benchmark names: " + this._testBenchmarks);
-	println("Benchmark sizes: " + this._testInputSizes);
+      for (var inputSize of this._testInputSizes) {
+        if (RosettaBenchmarkSet.isSizeSupported(benchName, inputSize)) {
+          print(" " + inputSize);
+        }
+      }
 
-	for (var benchName of this._testBenchmarks) {
+      println();
+    }
+  }
 
-		print(benchName + ":");
+  /*** IMPLEMENTATIONS ***/
 
-		for (var inputSize of this._testInputSizes) {
+  _getInstancesPrivate() {
+    var instances = [];
 
-			if (RosettaBenchmarkSet.isSizeSupported(benchName, inputSize)) {
-				print(" " + inputSize);
-			}
-		}
+    for (var benchName of this._testBenchmarks) {
+      for (var inputSize of this._testInputSizes) {
+        if (!RosettaBenchmarkSet.isSizeSupported(benchName, inputSize)) {
+          continue;
+        }
 
-		println();
-	}
-}
+        instances.push(new RosettaBenchmarkInstance(benchName, inputSize));
+      }
+    }
 
-/*** IMPLEMENTATIONS ***/
-
-RosettaBenchmarkSet.prototype._getInstancesPrivate = function () {
-
-	var instances = [];
-
-	for (var benchName of this._testBenchmarks) {
-
-		for (var inputSize of this._testInputSizes) {
-
-			if (!RosettaBenchmarkSet.isSizeSupported(benchName, inputSize)) {
-				continue;
-			}
-
-			instances.push(new RosettaBenchmarkInstance(benchName, inputSize));
-
-		}
-	}
-
-	return instances;
+    return instances;
+  }
 }
