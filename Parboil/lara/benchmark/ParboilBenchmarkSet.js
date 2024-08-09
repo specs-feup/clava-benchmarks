cebea259ef86e08bb1bcc96feed8551d72d5ee1a
João Bispo <jbispo@fe.up.pt>
2024-08-09 15:49:11 +0100

    Updates Parboil to support Lara-JS

--- Parboil/lara/benchmark/ParboilBenchmarkSet.js
+++ Parboil/lara/benchmark/ParboilBenchmarkSet.js
@@ -8,135 +8,184 @@ laraImport("lara.util.StringSet");
 /**
  * C-version of the Parboil benchmarks.
  */
-function ParboilBenchmarkSet() {
-	// Parent constructor
-	BenchmarkSet.call(this, "ParboilBenchmarkSet");
+class ParboilBenchmarkSet extends BenchmarkSet {
+  constructor() {
+    // Parent constructor
+    super("ParboilBenchmarkSet");
 
-	this._testBenchmarks = ["bfs", "cutcp", "histo", "lbm", "mri-gridding", "mri-q", "sad", "sgemm", "spmv", "stencil", "tpacf"];
-	this._testInputSizes = ["1M", "NY", "SF", "UT", "large", "small", "short", "long", "default", "medium"];
-}
-// Inheritance
-ParboilBenchmarkSet.prototype = Object.create(BenchmarkSet.prototype);
+    this._testBenchmarks = [
+      "bfs",
+      "cutcp",
+      "histo",
+      "lbm",
+      "mri-gridding",
+      "mri-q",
+      "sad",
+      "sgemm",
+      "spmv",
+      "stencil",
+      "tpacf",
+    ];
+    this._testInputSizes = [
+      "1M",
+      "NY",
+      "SF",
+      "UT",
+      "large",
+      "small",
+      "short",
+      "long",
+      "default",
+      "medium",
+    ];
+  }
 
-/*
- * Available benchmarks
- */
-ParboilBenchmarkSet._benchmarkNames = new PredefinedStrings("benchmark name", true, ["bfs", "cutcp", "histo", "lbm", "mri-gridding", 
-							"mri-q", "sad", "sgemm", "spmv", "stencil", "tpacf"]);
+  /*
+   * Available benchmarks
+   */
+  static _benchmarkNames = new PredefinedStrings("benchmark name", true, [
+    "bfs",
+    "cutcp",
+    "histo",
+    "lbm",
+    "mri-gridding",
+    "mri-q",
+    "sad",
+    "sgemm",
+    "spmv",
+    "stencil",
+    "tpacf",
+  ]);
 
-/*
- * Available sizes
- */
-ParboilBenchmarkSet._inputSizes = new PredefinedStrings("input size", true, ["1M", "NY", "SF", "UT", "large", "small", "short", "long", 
-							"default", "medium"]);
+  /*
+   * Available sizes
+   */
+  static _inputSizes = new PredefinedStrings("input size", true, [
+    "1M",
+    "NY",
+    "SF",
+    "UT",
+    "large",
+    "small",
+    "short",
+    "long",
+    "default",
+    "medium",
+  ]);
 
+  /**
+   * @return {lara.util.PredefinedStrings} Names of the available benchmarks.
+   */
+  static getBenchmarkNames = function () {
+    return ParboilBenchmarkSet._benchmarkNames;
+  };
 
-/**
- * @return {lara.util.PredefinedStrings} Names of the available benchmarks.
- */
-ParboilBenchmarkSet.getBenchmarkNames = function () {
-	return ParboilBenchmarkSet._benchmarkNames;
-}
+  /**
+   * @return {lara.util.PredefinedStrings} Available input sizes (some benchmarks might not support all sizes).
+   */
+  static getInputSizes = function () {
+    return ParboilBenchmarkSet._inputSizes;
+  };
 
-/**
- * @return {lara.util.PredefinedStrings} Available input sizes (some benchmarks might not support all sizes).
- */
-ParboilBenchmarkSet.getInputSizes = function () {
-	return ParboilBenchmarkSet._inputSizes;
-}
+  static isSizeSupported = function (benchName, inputSize) {
+    // Check if name and inputSize are valid
+    ParboilBenchmarkSet.getBenchmarkNames().test(benchName);
+    ParboilBenchmarkSet.getInputSizes().test(inputSize);
 
-ParboilBenchmarkSet.isSizeSupported = function (benchName, inputSize) {
-	// Check if name and inputSize are valid
-	ParboilBenchmarkSet.getBenchmarkNames().test(benchName);
-	ParboilBenchmarkSet.getInputSizes().test(inputSize);
+    if (benchName == "bfs") {
+      return (
+        inputSize == "1M" ||
+        inputSize == "NY" ||
+        inputSize == "SF" ||
+        inputSize == "UT"
+      );
+    }
+    if (benchName == "cutcp") {
+      return inputSize == "large" || inputSize == "small";
+    }
+    if (benchName == "histo") {
+      return inputSize == "large" || inputSize == "default";
+    }
+    if (benchName == "lbm") {
+      return inputSize == "long" || inputSize == "short";
+    }
+    if (benchName == "mri-gridding") {
+      return inputSize == "small";
+    }
+    if (benchName == "mri-q") {
+      return inputSize == "large" || inputSize == "small";
+    }
+    if (benchName == "sad") {
+      return inputSize == "large" || inputSize == "default";
+    }
+    if (benchName == "sgemm") {
+      return inputSize == "medium" || inputSize == "small";
+    }
+    if (benchName == "spmv") {
+      return (
+        inputSize == "large" || inputSize == "small" || inputSize == "medium"
+      );
+    }
+    if (benchName == "stencil") {
+      return inputSize == "small" || inputSize == "default";
+    }
+    if (benchName == "tpacf") {
+      return (
+        inputSize == "large" || inputSize == "small" || inputSize == "medium"
+      );
+    }
 
-	if (benchName == "bfs") {
-		return (inputSize == "1M" || inputSize == "NY" || inputSize == "SF" || inputSize == "UT");
-	} 
-	if (benchName == "cutcp") {
-		return (inputSize == "large" || inputSize == "small");
-	} 
-	if (benchName == "histo") {
-		return (inputSize == "large" || inputSize == "default");
-	} 
-	if (benchName == "lbm") {
-		return (inputSize == "long" || inputSize == "short");
-	} 
-	if (benchName == "mri-gridding") {
-		return (inputSize == "small");
-	} 
-	if (benchName == "mri-q") {
-		return (inputSize == "large" || inputSize == "small");
-	} 
-	if (benchName == "sad") {
-		return (inputSize == "large" || inputSize == "default");
-	} 
-	if (benchName == "sgemm") {
-		return (inputSize == "medium" || inputSize == "small");
-	} 
-	if (benchName == "spmv") {
-		return (inputSize == "large" || inputSize == "small" || inputSize == "medium");
-	} 
-	if (benchName == "stencil") {
-		return (inputSize == "small" || inputSize == "default");
-	} 
-	if (benchName == "tpacf") {
-		return (inputSize == "large" || inputSize == "small" || inputSize == "medium");
-	} 
+    return false;
+  };
 
-	return false;
-}
+  setBenchmarks = function () {
+    this._testBenchmarks = ParboilBenchmarkSet.getBenchmarkNames().parse(
+      arrayFromArgs(arguments)
+    );
+  };
 
-ParboilBenchmarkSet.prototype.setBenchmarks = function () {
-	this._testBenchmarks = ParboilBenchmarkSet.getBenchmarkNames().parse(arrayFromArgs(arguments));
-}
+  setInputSizes = function () {
+    this._testInputSizes = ParboilBenchmarkSet.getInputSizes().parse(
+      arrayFromArgs(arguments)
+    );
+  };
 
-ParboilBenchmarkSet.prototype.setInputSizes = function () {
-	this._testInputSizes = ParboilBenchmarkSet.getInputSizes().parse(arrayFromArgs(arguments));
-}
+  /**
+   * Prints the current Parboil benchmark set.
+   */
+  print = function () {
+    println("BenchmarkSet: " + this.getName());
+    println("Benchmark names: " + this._testBenchmarks);
+    println("Benchmark sizes: " + this._testInputSizes);
 
+    for (var benchName of this._testBenchmarks) {
+      print(benchName + ":");
 
-/**
- * Prints the current Parboil benchmark set.
- */
-ParboilBenchmarkSet.prototype.print = function () {
-	println("BenchmarkSet: " + this.getName());
-	println("Benchmark names: " + this._testBenchmarks);
-	println("Benchmark sizes: " + this._testInputSizes);
+      for (var inputSize of this._testInputSizes) {
+        if (ParboilBenchmarkSet.isSizeSupported(benchName, inputSize)) {
+          print(" " + inputSize);
+        }
+      }
 
-	for (var benchName of this._testBenchmarks) {
+      println();
+    }
+  };
 
-		print(benchName + ":");
+  /*** IMPLEMENTATIONS ***/
 
-		for (var inputSize of this._testInputSizes) {
+  _getInstancesPrivate = function () {
+    var instances = [];
 
-			if (ParboilBenchmarkSet.isSizeSupported(benchName, inputSize)) {
-				print(" " + inputSize);
-			}
-		}
+    for (var benchName of this._testBenchmarks) {
+      for (var inputSize of this._testInputSizes) {
+        if (!ParboilBenchmarkSet.isSizeSupported(benchName, inputSize)) {
+          continue;
+        }
 
-		println();
-	}
-}
+        instances.push(new ParboilBenchmarkInstance(benchName, inputSize));
+      }
+    }
 
-/*** IMPLEMENTATIONS ***/
-
-ParboilBenchmarkSet.prototype._getInstancesPrivate = function () {
-
-	var instances = [];
-
-	for (var benchName of this._testBenchmarks) {
-
-		for (var inputSize of this._testInputSizes) {
-
-			if (!ParboilBenchmarkSet.isSizeSupported(benchName, inputSize)) {
-				continue;
-			}
-
-			instances.push(new ParboilBenchmarkInstance(benchName, inputSize));
-
-		}
-	}
-
-	return instances;
+    return instances;
+  };
 }
